a0dd7ac58e491e6833a494558abef0716c8b115c
Imran-2022 <mdimranulhaque202@gmail.com>
2024-03-11 01:12:21 +0600

    edit musician completed

--- musician/urls.py
+++ musician/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('add_musician/',views.add_musician,name="add_musician"),
+    path('edit_musician/<int:id>',views.edit_musician,name="edit_musician"),
 ]
--- musician/views.py
+++ musician/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from . import forms
+from album.models import Musician
 # Create your views here.
 
 def add_musician(request):
@@ -11,3 +12,15 @@ def add_musician(request):
     else: 
         musician_form=forms.MusicianForm()
     return render(request, 'musician.html',{'form':musician_form})
+
+
+def edit_musician(request,id):
+    post = Musician.objects.get(pk=id)
+    post_form=forms.MusicianForm(instance=post) 
+
+    if request.method=='POST': 
+        post_form=forms.MusicianForm(request.POST,instance=post) 
+        if post_form.is_valid(): 
+            post_form.save()
+            return redirect('homepage') 
+    return render(request, 'musician.html',{'form':post_form})
